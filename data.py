0110247d9e2027eab76003365a63fed44801a571
Tron <tusiime.ronald@gmail.com>
2024-07-17 17:46:46 +0300

    Initial Commit

---
+++ data.py
@@ -0,0 +1,22 @@
+def Articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Python is Fun", 
+            "content": "This article explains why Python is a great programming language.",  
+            "author": "John Doe"
+        },
+        {
+            "id": 2,
+            "title": "Python is Fun", 
+            "content": "This article explains why Python is a great programming language.",  
+            "author": "John Doe"
+        },
+        {
+            "id": 3,
+            "title": "Python is Fun", 
+            "content": "This article explains why Python is a great programming language.",  
+            "author": "John Doe"
+        }
+    ]
+    return articles
